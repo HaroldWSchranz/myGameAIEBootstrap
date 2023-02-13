85dab8ff4fc38aeaee785d7a6c6763d471b681c5
HaroldWSchranz <Harold.Schranz@gmail.com>
2023-02-13 16:41:47 +1100

    Created FixedTimestepTutorial  and Project2DTinker projects

---
+++ Project2DTinker/Application2D.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Application.h"
+#include "Renderer2D.h"
+
+class Application2D : public aie::Application {
+public:
+
+	Application2D();
+	virtual ~Application2D();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual void update(float deltaTime);
+	virtual void draw();
+
+protected:
+
+	aie::Renderer2D*	m_2dRenderer;
+	aie::Texture*		m_texture;
+	aie::Texture*		m_shipTexture;
+	aie::Font*			m_font;
+
+	aie::Texture*		m_carTexture;
+	aie::Texture*		m_rockTexture;
+
+	float m_timer;
+};
